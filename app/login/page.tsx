2896405df189c71c350fd044600da3d53b6d1cb0
Suhel Tamboli <suhelmtamboli9@gmail.com>
2026-02-09 16:52:09 +0530

    added shadcn ui component library and created login form ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,61 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 export default function Login() {
-  return <div className="flex justify-center">Login Form</div>;
+  return (
+    <div className="flex justify-center pt-24">
+      <Card className="w-full max-w-sm">
+        <CardHeader className="flex justify-center">
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full cursor-pointer">
+            Login
+          </Button>
+          <p>
+            Don&apos;t Have an Account?{" "}
+            <Link className="underline" href={"/register"}>
+              Register
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
